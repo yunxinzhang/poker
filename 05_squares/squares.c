276d0a0bef6b2162d50318336fa08456cfe873ec
Coursera Learner <nobody@nowhere.nul>
2018-09-16 09:01:36 +0000

    update

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -18,7 +18,7 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
       //    ((y is between y_offset and y_offset + size2) AND
       //     x is equal to either x_offset OR x_offset + size2 -1)
       // if so, print a *
-		if(((x>= x_offset && x<= x_offset+size2) && (y==y_offset || y==y_offset +size2-1)) || ((y>= y_offset && y<= y_offset+size2) && (x==x_offset || x==x_offset+size2-1)))
+		if(((x>= x_offset && x< x_offset+size2) && (y==y_offset || y==y_offset +size2-1)) || ((y>= y_offset && y< y_offset+size2) && (x==x_offset || x==x_offset+size2-1)))
 			printf("*");
       //if not,
 		else if((x<size1 && (y==0 || y==size1-1)) || (y<size1 && (x==0 ||x == size1-1)))

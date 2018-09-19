dddcabe6944635d1383df9054076c4aa7d4ffad3
Coursera Learner <nobody@nowhere.nul>
2018-09-19 19:50:19 +0000

    u

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -6,7 +6,7 @@
 
 void assert_card_valid(card_t c) {
 	if( c.value < 2 || c.value > 14 || c.suit < 0 || c.suit > 3 )
-		printf("those are not valid values\n");
+		printf("Those are not valid values\n");
 }
 
  const char * p0 = "STRAIGHT_FLUSH";

ff651653802a497e48d7737871f9456555f55d4d
Coursera Learner <nobody@nowhere.nul>
2018-09-21 03:09:44 +0000

    maxSeq delete useless var

--- 16_subseq/maxSeq.c
+++ 16_subseq/maxSeq.c
@@ -2,7 +2,6 @@
 size_t maxSeq(int * array, size_t n){
     if(n==0)return 0;
     int maxlen = 1;
-    int start = 0;
     int len = 1;
     for(int i=0; i<n-1; ++i){
         if(array[i+1]>array[i]){
@@ -11,7 +10,6 @@ size_t maxSeq(int * array, size_t n){
     		maxlen = len ;
            }
         }else{
-           start = i+1;
 	   len = 1;
         }
     }

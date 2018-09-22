2e13e41a5dc493049839f1caf39f67679811cee6
Coursera Learner <nobody@nowhere.nul>
2018-09-22 17:54:50 +0000

    deck start from 2

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -16,26 +16,33 @@ int deck_contains(deck_t * d, card_t c) {
   }
   return 0;
 }
-#include <time.h>
+//#include <time.h>
 void shuffle(deck_t * d){
   int rnd = 0;
   card_t * cp = NULL;
-  srand(clock());
+//  srand(clock());
   for(int i=0; i<d->n_cards; ++i){
 	rnd = random() % (d->n_cards-i) + i;
 	cp = d->cards[i];
 	d->cards[i] = d->cards[rnd];
 	d->cards[rnd] = cp; 
   }
-  cp = NULL;
+//  cp = NULL;
 }
 
 void assert_full_deck(deck_t * d) {
-  int nc[52] = {0};
+  int nc[52]={0};
+//  for(int i=0; i<52; ++i){
+//	nc[i]=0;
+//	printf("%d",nc[i]);
+//  }
   int ind = 0;
   for(int i=0; i<d->n_cards; ++i){
-    ind = d->cards[i]->suit * 13 + d->cards[i]->value;
-    nc[ind]++;
-    if(nc[ind]>1)exit(EXIT_FAILURE);
+    ind = d->cards[i]->suit * 13 + d->cards[i]->value -2;
+    nc[ind]+=1;
+//	printf("%d,", ind);    
+    if(nc[ind]>1){
+	exit(EXIT_FAILURE);
+    }
   }
 }

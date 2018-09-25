9e52e6144537cc3c72503f13fa0643d63b8819f5
Coursera Learner <nobody@nowhere.nul>
2018-09-25 02:43:28 +0000

    SC decode

---
+++ 25_break_encr/decrypt.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+int max_num( int * nums, int n){
+   assert(n>0);
+   int max = nums[0], idx = 0;
+   for(size_t i=1; i<n ;++i){
+	if(nums[i] > max){
+  	   idx = i;
+	   max = nums[i];
+        }
+   }
+   return idx;
+}
+void decrypt(FILE * fp){
+  int c;
+  int chs[26] = {0};
+  size_t idx = 0;
+  while( (c = fgetc(fp))!=EOF){
+	idx = c - 'a';
+	if(idx>=0 && idx<26)
+		++chs[c-'a'];
+  }
+  printf("%d\n", max_num(chs, 26));
+}
+
+int main(int argc, char ** argv){
+  if(argc!=2){
+	fprintf(stderr, "Usage: break inputFileName\n");
+	return EXIT_FAILURE;
+  }
+
+  FILE * fp = fopen(argv[1], "r");
+  if(fp==NULL){
+	perror("Could not open file!");
+	return EXIT_FAILURE;
+  }
+  decrypt(fp);
+  if(fclose(fp) != 0){
+	perror("Failed to close the input file!");
+	return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+
+}

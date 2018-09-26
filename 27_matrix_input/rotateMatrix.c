36671f2ba237cb2001fd555a9c06fe692bacfbfc
Coursera Learner <nobody@nowhere.nul>
2018-09-26 05:17:09 +0000

    file to short

--- 27_matrix_input/rotateMatrix.c
+++ 27_matrix_input/rotateMatrix.c
@@ -47,6 +47,10 @@ int main(int argc, char** argv){
 			exit(EXIT_FAILURE);
 		}
 	}
+	if(cnt<109){
+		perror("Too short");
+		exit(EXIT_FAILURE);
+	}
 	rotate(matrix);
 	for(int i=0; i<10; ++i){
 		for(int j=0; j<10; ++j)

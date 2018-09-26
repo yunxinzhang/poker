7f0194bc81eff49c3e70b9fb4dea5eb3f726aaaf
Coursera Learner <nobody@nowhere.nul>
2018-09-26 10:45:00 +0000

    read sort

--- 30_sort_lines/sortLines.c
+++ 30_sort_lines/sortLines.c
@@ -15,10 +15,55 @@ int stringOrder(const void * vp1, const void * vp2) {
 void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
+void copyStr(char ** strs, FILE * fp, size_t sz, size_t ssz){
+
+  int c;
+	strs = malloc(sz * sizeof(*strs)); 
+ 	for(int i=0; i<sz; ++i){
+		strs[i] = malloc( ssz * sizeof(**strs));
+	}	
+	int i=0, j=0;
+	while( (c = getc(fp))!= EOF ){
+		strs[i] = realloc(strs[i], (j+10)*sizeof(**strs));
+		strs[i][j++] = c;	
+		if(c=='\n'){
+			strs[i][j]='\0';
+			++i; 
+			j=0;
+			if(i>=sz) {
+				sz *= 2;
+				strs = realloc(strs, sz*sizeof(*strs));
+				for(int k=0; k<sz/2; ++k){
+					strs[sz/2+k] = malloc(ssz * sizeof(**strs));
+				}
+			}
+		}
+	}
+  for(int i=0; i<sz; ++i){
+	printf("%s", strs[i]);
+  }
+	for(int i=0; i<sz; ++i) free(strs[i]);
+	free(strs);
+} 
 
 int main(int argc, char ** argv) {
   
   //WRITE YOUR CODE HERE!
-  
+  char ** strs = NULL;
+  size_t sz=8, ssz=1024;
+  if(argc==1)
+  	copyStr(strs,stdin,sz,ssz);
+  else{
+	for(int i=1; i<argc; ++i){
+		FILE *fp = fopen(argv[i], "r");
+		if(fp==NULL)exit(EXIT_FAILURE);
+		copyStr(strs, fp, sz, ssz);
+		int  fc = fclose(fp);
+ 		if(fc!=0){
+			perror("close file error");
+			exit(EXIT_FAILURE);
+		}
+	}	
+  }
   return EXIT_SUCCESS;
 }

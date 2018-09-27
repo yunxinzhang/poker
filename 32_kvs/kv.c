6dc08b132eb4fe5dfe98a171793959194c38795d
Coursera Learner <nobody@nowhere.nul>
2018-09-27 05:46:16 +0000

    read kv

--- 32_kvs/kv.c
+++ 32_kvs/kv.c
@@ -7,16 +7,63 @@
 
 kvarray_t * readKVs(const char * fname) {
   //WRITE ME
+  FILE *fp = fopen(fname ,"r");
+  if(fp==NULL){
+	perror("Open file error!");
+	exit(EXIT_FAILURE);
+  }
+  int nkv = 128;
+  kvarray_t * arrp = malloc(sizeof(*arrp));
+  arrp->npair = 0;
+  arrp->kvarr = malloc(nkv*sizeof(*(arrp->kvarr)));
+  char * line = NULL;
+  size_t sz = 0;
+  int used_kv = 0;  
+  while(getline(&line, &sz, fp)!=-1){
+	if( strchr(line, '=') == NULL) continue;
+	int len = strchr(line, '=') - line;
+	if(used_kv>=nkv){
+		nkv *= 2;
+		arrp->kvarr = realloc(arrp->kvarr, nkv*sizeof(*(arrp->kvarr)));
+ 	}	
+	arrp->kvarr[used_kv].key = malloc(strlen(line));
+	strncpy(arrp->kvarr[used_kv].key, line, len);
+ 	arrp->kvarr[used_kv].key[len] = '\0';
+	arrp->kvarr[used_kv].value = malloc(strlen(line));
+	strcpy(arrp->kvarr[used_kv].value, strchr(line,'=')+1);
+	strchr(arrp->kvarr[used_kv].value,'\n')[0] = '\0';
+	++used_kv;
+  }
+  arrp->npair = used_kv;
+  int cls = fclose(fp);
+  if(cls!=0){
+	perror("Close file failed!");
+ 	exit(EXIT_FAILURE);
+  }
+  return arrp;
 }
 
 void freeKVs(kvarray_t * pairs) {
   //WRITE ME
+  for(int i=0; i< pairs->npair; ++i){
+	free(pairs->kvarr[i].key);
+	free(pairs->kvarr[i].value);
+  }
+  free(pairs->kvarr);
+  free(pairs);
 }
 
 void printKVs(kvarray_t * pairs) {
   //WRITE ME
+  for(int i=0; i< pairs->npair; ++i){
+	printf("key = '%s' value = '%s'\n", pairs->kvarr[i].key, pairs->kvarr[i].value);
+  }
 }
 
 char * lookupValue(kvarray_t * pairs, const char * key) {
   //WRITE ME
+  for(int i=0; i<pairs->npair; ++i){
+	if(strcmp(key, pairs->kvarr[i].key)==0)return pairs->kvarr[i].value;
+  }
+  return NULL;
 }
--- 32_kvs/kv.h
+++ 32_kvs/kv.h
@@ -4,14 +4,15 @@
 
 struct _kvpair_t { 
   //DEFINE ME!
-
+  char * key;
+  char * value;
 };
 typedef struct _kvpair_t kvpair_t;
 
 struct _kvarray_t { 
   //DEFINE ME!
-
-
+  size_t npair;
+  kvpair_t * kvarr;
 };
 typedef struct _kvarray_t kvarray_t;
 

22c37b7a4ccb8f15fd2f3eed286dc61baf0c81e8
Coursera Learner <nobody@nowhere.nul>
2018-09-28 07:26:20 +0000

    ;;;;;

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -89,7 +89,7 @@ void free_deck(deck_t * deck){
 	}
 	free(deck->cards);
 	free(deck);
-} ;
+} 
 deck_t * build_remaining_deck(deck_t ** hands, size_t n_hands){
 	deck_t * ex = malloc(sizeof(deck_t));
 	ex->cards = NULL;

9f52728f177b885b42080e286561721632e2e25f
Coursera Learner <nobody@nowhere.nul>
2018-09-29 19:37:45 +0000

    test1

--- c4prj3_finish/main.c
+++ c4prj3_finish/main.c
@@ -7,10 +7,70 @@
 #include "eval.h"
 #include "future.h"
 #include "input.h"
-
-
+#include <string.h>
 
 int main(int argc, char ** argv) {
   //YOUR CODE GOES HERE
+  int mn;
+  if(argc==2){
+	mn = 10000;
+  }else if(argc==3){
+	for(int i=0; i<strlen(argv[2]); ++i){
+		if(argv[2][i]>'9' || argv[2][i] <'0'){
+			perror("not a number");
+			exit(EXIT_FAILURE);
+		}
+	}
+	mn = atoi((const char*) &argv[2] );
+  }else{
+	perror("wrong args");
+	exit(EXIT_FAILURE);
+  }
+// read_input 
+   FILE *f = fopen(argv[1], "r");
+   if(f==NULL){
+	perror("Open file failed");
+	exit(EXIT_FAILURE);
+   }
+   future_cards_t  fcp ;
+   fcp.n_decks = 0;
+   fcp.decks = NULL;
+   size_t n_hands=0;
+   deck_t ** allhand =   read_input(f, &n_hands, &fcp);
+   fclose(f);
+// remain cards
+   deck_t * remain = build_remaining_deck(allhand,n_hands);
+   int * cnt_wins = malloc( (n_hands+1) * sizeof(int));
+   for(int i=0; i<= n_hands; ++i) cnt_wins[i] = 0;
+   for(int i=0; i<mn; ++i){
+	shuffle(remain);
+	future_cards_from_deck(remain, &fcp);
+	deck_t * maxhand = allhand[0];
+	int idx_max =0;
+	int max_cnt =1;
+	for(int j=1; j<n_hands; ++j){
+	   if(compare_hands(allhand[j],maxhand)>0){
+		maxhand = allhand[j];
+		idx_max = j;
+		max_cnt =1;
+	   }else if(compare_hands(allhand[j],maxhand)==0){
+		max_cnt++;
+	   }
+	}
+	if(max_cnt >1)
+		cnt_wins[n_hands]++;
+	else 
+		cnt_wins[idx_max]++;
+   }	
+   for(int i=0; i<n_hands; ++i){
+	printf("Hand %d won %u / %u times (%.2f%%)\n", i, cnt_wins[i], mn, 1.0*cnt_wins[i]/mn);
+   }
+
+   printf("And there were %u ties\n", cnt_wins[n_hands]);
+   free(cnt_wins);
+   for(int i=0; i<n_hands; ++i)
+  	 free(allhand[i]);
+   free(allhand);
+
   return EXIT_SUCCESS;
 }
